17e9576bd43babb44695cf6a85c573d656dd62f1
coderoo7 <shubhamyadav.70007@gmail.com>
2021-01-01 22:44:05 +0530

    Anonymous login & app privacy policy

--- src/modules/firebaseAuthUI.js
+++ src/modules/firebaseAuthUI.js
@@ -10,8 +10,15 @@ const firebaseAuthUI = (()=>{
             firebase.auth.EmailAuthProvider.PROVIDER_ID,
             firebase.auth.GoogleAuthProvider.PROVIDER_ID,
             firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-            firebase.auth.PhoneAuthProvider.PROVIDER_ID
+            firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+            firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
         ],
+        // Terms of service url/callback.
+        tosUrl: 'https://www.termsofservicegenerator.net/live.php?token=4TkFemnB82ClTdd7H4ipTRpbTKAH7WXd',
+        // Privacy policy url/callback.
+        privacyPolicyUrl: function() {
+            window.location.assign('https://www.privacypolicygenerator.info/live.php?token=975oEjhxtmpw140WAo50hgns267X0HaG');
+        }
     }
     
     // Initialize the FirebaseUI Widget using Firebase.
