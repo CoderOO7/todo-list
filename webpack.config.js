45253f5bd6f1e2ebe2092a17c581246f73615fb2
CoderOO7 <shubhamyadav.70007@gmail.com>
2021-01-29 23:04:54 +0530

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,51 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    module: {
-        rules: [
-            {
-            test: /\.css$/i,
-            use: ["style-loader","css-loader"],
-            },
-        ],
-    },
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    }
-}
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules[\\/]webpack[\\/]buildin/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  targets: {
+                    node: "current",
+                    browsers: [
+                      "last 2 versions",
+                      "safari >= 7",
+                      "firefox >=64",
+                    ],
+                  },
+                },
+              ],
+            ],
+            plugins: [
+              [
+                "@babel/plugin-transform-runtime",
+                {
+                  regenerator: true,
+                  // "corejs": 3 // or 2; if polyfills needed
+                },
+              ],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
