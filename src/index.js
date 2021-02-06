e636bcafea450c5bf3b2256bed3f43d8ba5a24a4
CoderOO7 <shubhamyadav.70007@gmail.com>
2021-02-06 17:19:13 +0530

    chore: script changes & integrate babel with webpack

--- src/index.js
+++ src/index.js
@@ -1,72 +1,89 @@
-import {todoAppFirebaseStorage} from './modules/storage/firebaseStorage.js';
-import {projectController} from './modules/logic/projects.js';
-import {todoController} from './modules/logic/todos.js';
-import {todoFilterDom, userAuthDom} from './modules/domStuff.js';
-import {renderDom} from './modules/render.js';
-import firebase from './modules/firebase.js';
-import firebaseAuthUI from './modules/firebaseAuthUI';
-import './modules/staticEventsListeners.js';
-import {PROJECT_COLLECTION_NAME, TODO_COLLECTION_NAME} from './modules/constants.js';
+import { todoAppFirebaseStorage } from "./modules/storage/firebaseStorage.js";
+import { projectController } from "./modules/logic/projects.js";
+import { todoController } from "./modules/logic/todos.js";
+import { todoFilterDom, userAuthDom } from "./modules/domStuff.js";
+import { renderDom } from "./modules/render.js";
+import firebase from "./modules/firebase.js";
+import firebaseAuthUI from "./modules/firebaseAuthUI";
+import "./modules/staticEventsListeners.js";
+import {
+  PROJECT_COLLECTION_NAME,
+  TODO_COLLECTION_NAME,
+} from "./modules/constants.js";
 
-(function initApp(){
+(function initApp() {
+  console.log("initApp");
+  //Observer for change in user sign-in state
+  firebase.auth().onAuthStateChanged(
+    function (user) {
+      if (user) {
+        // User is signed in.
+        const userName = user.displayName
+          ? user.displayName
+          : user.email
+          ? user.email
+          : "User";
+        console.log(
+          `%cWelcome :) ${userName}`,
+          "color:green;font-family:system-ui;font-size:2rem;-webkit-text-stroke: 1px black;font-weight:bold"
+        );
 
-    //Observer for change in user sign-in state
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-            // User is signed in.
-            const userName = user.displayName ? user.displayName : user.email ? user.email : "User";
-            console.log(`%cWelcome :) ${userName}`,"color:green;font-family:system-ui;font-size:2rem;-webkit-text-stroke: 1px black;font-weight:bold");
-            
-            user.getIdToken().then(function(accessToken) {
-                (async () =>{
-                    userAuthDom.handleLogIn(userName);
-                    document.querySelector('.header__signout-btn').onclick = () => {
-                        firebase.auth().signOut();
-                        window.location.reload();
-                    };
-    
-                    {   /* Fetch data from firestore */
-                        {
-                            //Fetch projects
-                            await todoAppFirebaseStorage.getItems(PROJECT_COLLECTION_NAME)
-                            .then((projects)=>{
-                                projects.forEach((project)=>{
-                                    projectController.addProjectToStore(project.data());
-                                })
-                            })
-                        
-                            //Fetch todos
-                            await todoAppFirebaseStorage.getItems(TODO_COLLECTION_NAME)
-                            .then((todos)=>{
-                                todos.forEach((todo)=>{
-                                    todoController.addTodoTaskToStore(todo.data());
-                                })
-                            })
+        user.getIdToken().then(function (accessToken) {
+          (async () => {
+            userAuthDom.handleLogIn(userName);
+            document.querySelector(".header__signout-btn").onclick = () => {
+              firebase.auth().signOut();
+              window.location.reload();
+            };
 
-                        }
-                    }
-    
-                    //Render DOM
-                    renderDom.todoFilter.list();
-                    renderDom.project.list();
-                    renderDom.todo.list();
-        
-                    // Activating default tab on initial load
-                    const event = new MouseEvent('click');
-                    const _todayTaskFilterTab = document.querySelector(".sidenav__filter-item[data-index='0']");
-                    _todayTaskFilterTab.addEventListener('click',todoFilterDom.activateFilterTab);
-                    _todayTaskFilterTab.dispatchEvent(event);
+            {
+              /* Fetch data from firestore */
+              {
+                //Fetch projects
+                await todoAppFirebaseStorage
+                  .getItems(PROJECT_COLLECTION_NAME)
+                  .then((projects) => {
+                    projects.forEach((project) => {
+                      projectController.addProjectToStore(project.data());
+                    });
+                  });
 
-                })();
+                //Fetch todos
+                await todoAppFirebaseStorage
+                  .getItems(TODO_COLLECTION_NAME)
+                  .then((todos) => {
+                    todos.forEach((todo) => {
+                      todoController.addTodoTaskToStore(todo.data());
+                    });
+                  });
+              }
+            }
 
-            })} else {
-            // User is signed out.
-            userAuthDom.handleLogOut();
-            firebaseAuthUI.loadUI();
-        }
-        
-    }, function(error) {
-        console.error(error);
-    });
-    
+            //Render DOM
+            renderDom.todoFilter.list();
+            renderDom.project.list();
+            renderDom.todo.list();
+
+            // Activating default tab on initial load
+            const event = new MouseEvent("click");
+            const _todayTaskFilterTab = document.querySelector(
+              ".sidenav__filter-item[data-index='0']"
+            );
+            _todayTaskFilterTab.addEventListener(
+              "click",
+              todoFilterDom.activateFilterTab
+            );
+            _todayTaskFilterTab.dispatchEvent(event);
+          })();
+        });
+      } else {
+        // User is signed out.
+        userAuthDom.handleLogOut();
+        firebaseAuthUI.loadUI();
+      }
+    },
+    function (error) {
+      console.error(error);
+    }
+  );
 })();
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,21 @@
 const path = require("path");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    main: "./src/index.js",
+  },
   devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+    // writeToDisk: true
+  },
   module: {
     rules: [
       {
         test: /\.m?js$/,
-        exclude: /node_modules[\\/]webpack[\\/]buildin/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              [
-                "@babel/preset-env",
-                {
-                  targets: {
-                    node: "current",
-                    browsers: [
-                      "last 2 versions",
-                      "safari >= 7",
-                      "firefox >=64",
-                    ],
-                  },
-                },
-              ],
-            ],
-            plugins: [
-              [
-                "@babel/plugin-transform-runtime",
-                {
-                  regenerator: true,
-                  // "corejs": 3 // or 2; if polyfills needed
-                },
-              ],
-            ],
-          },
-        },
+        exclude: /node_modules/,
+        use: ["babel-loader"],
       },
       {
         test: /\.css$/i,
@@ -44,8 +23,16 @@ module.exports = {
       },
     ],
   },
+  /* plugins: [
+    new HtmlWebpackPlugin({
+      template: "./dist/index.html",
+    })
+  ], */
   output: {
-    filename: "main.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  /*  optimization: {
+    splitChunks: { chunks: "all" }
+  }, */
 };
